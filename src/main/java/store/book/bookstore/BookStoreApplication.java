e51a32a3eef4ab6c08146246b46debdfdb64e392
MYKOLA YAKYMCHUK <m.yakymchuk5@gmail.com>
2025-12-03 01:37:23 +0100

    replaced the date annotation with a getter, setter. removed the numbering of objects

--- src/main/java/store/book/bookstore/BookStoreApplication.java
+++ src/main/java/store/book/bookstore/BookStoreApplication.java
@@ -21,26 +21,27 @@ public class BookStoreApplication {
     @Bean
     public CommandLineRunner commandLineRunner() {
         return args -> {
-            Book book1 = new Book();
-            book1.setTitle("Warriors. Into the wild");
-            book1.setAuthor("Erine Hunter");
-            book1.setIsbn("978-0062366962");
-            book1.setPrice(new BigDecimal("7.99"));
-            book1.setDescription(
+            Book warriors = new Book();
+            warriors.setTitle("Warriors. Into the wild");
+            warriors.setAuthor("Erine Hunter");
+            warriors.setIsbn("978-0062366962");
+            warriors.setPrice(new BigDecimal("7.99"));
+            warriors.setDescription(
                     "The first spine-tingling episode in the planned Warriors series. "
                             + "Sure to appeal to followers of Brian Jacques’"
                             + " ongoing Redwall series.");
-            book1.setCoverImage("Firepaw.jpg");
-            bookService.save(book1);
+            warriors.setCoverImage("Firepaw.jpg");
+            bookService.save(warriors);
 
-            Book book2 = new Book();
-            book2.setTitle("The Da Vinci Code");
-            book2.setAuthor("Dan Brown");
-            book2.setIsbn("978-0307474278");
-            book2.setPrice(new BigDecimal("6.39"));
-            book2.setDescription("Blockbuster perfection.... A gleefully erudite suspense novel");
-            book2.setCoverImage("Da-Vinci-Code.jpg");
-            bookService.save(book2);
+            Book daVinciCode = new Book();
+            daVinciCode.setTitle("The Da Vinci Code");
+            daVinciCode.setAuthor("Dan Brown");
+            daVinciCode.setIsbn("978-0307474278");
+            daVinciCode.setPrice(new BigDecimal("6.39"));
+            daVinciCode.setDescription("Blockbuster perfection.... "
+                    + "A gleefully erudite suspense novel");
+            daVinciCode.setCoverImage("Da-Vinci-Code.jpg");
+            bookService.save(daVinciCode);
 
             System.out.println("All books in the database:");
             bookService.findAll().forEach(book ->
