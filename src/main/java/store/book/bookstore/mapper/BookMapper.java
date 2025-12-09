4399120a9dac0b21a27002ed3696b8e61bc2b785
MYKOLA YAKYMCHUK <m.yakymchuk5@gmail.com>
2025-12-09 21:40:11 +0100

    fix

--- src/main/java/store/book/bookstore/mapper/BookMapper.java
+++ src/main/java/store/book/bookstore/mapper/BookMapper.java
@@ -2,6 +2,7 @@ package store.book.bookstore.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import store.book.bookstore.dto.BookDto;
 import store.book.bookstore.dto.CreateBookRequestDto;
 import store.book.bookstore.model.Book;
@@ -13,5 +14,9 @@ public interface BookMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "deleted", ignore = true)
     Book toModel(CreateBookRequestDto requestDto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "deleted", ignore = true)
+    void updateBookFromDto(CreateBookRequestDto requestDto, @MappingTarget Book book);
 
 }
