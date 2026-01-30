3e9a6e138533720c615ea46f81f99cf7935c34cb
MYKOLA YAKYMCHUK <m.yakymchuk5@gmail.com>
2026-01-30 22:13:33 +0100

    Add JWT support

--- src/main/java/store/book/bookstore/controller/AuthenticationController.java
+++ src/main/java/store/book/bookstore/controller/AuthenticationController.java
@@ -11,9 +11,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import store.book.bookstore.dto.UserLoginRequestDto;
+import store.book.bookstore.dto.UserLoginResponseDto;
 import store.book.bookstore.dto.UserRegistrationRequestDto;
 import store.book.bookstore.dto.UserResponseDto;
 import store.book.bookstore.exception.RegistrationException;
+import store.book.bookstore.security.AuthenticationService;
 import store.book.bookstore.service.UserService;
 
 @Tag(name = "Authentication", description = "Endpoints for user authentication")
@@ -22,6 +25,7 @@ import store.book.bookstore.service.UserService;
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @Operation(
             summary = "Register a new user",
@@ -34,5 +38,16 @@ public class AuthenticationController {
             @Valid @RequestBody UserRegistrationRequestDto request)
             throws RegistrationException {
         return userService.register(request);
+    }
+
+    @Operation(
+            summary = "User authentication",
+            description = "Authenticate user and return JWT token"
+    )
+    @PostMapping("/login")
+    public UserLoginResponseDto login(
+            @Parameter(description = "User login credentials", required = true)
+            @Valid @RequestBody UserLoginRequestDto requestDto) {
+        return authenticationService.authenticate(requestDto);
     }
 }
