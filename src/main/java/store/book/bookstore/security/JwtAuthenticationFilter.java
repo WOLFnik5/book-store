081e89b673b3fef37effec23c0d3eb05d92bf861
MYKOLA YAKYMCHUK <m.yakymchuk5@gmail.com>
2026-01-31 13:59:40 +0100

    fix

--- src/main/java/store/book/bookstore/security/JwtAuthenticationFilter.java
+++ src/main/java/store/book/bookstore/security/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,7 +19,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    private static final String AUTHORIZATION_HEADER = "Authorization";
     private static final String BEARER_PREFIX = "Bearer ";
 
     private final JwtUtil jwtUtil;
@@ -43,7 +43,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     private String getToken(HttpServletRequest request) {
-        String bearToken = request.getHeader(AUTHORIZATION_HEADER);
+        String bearToken = request.getHeader(HttpHeaders.AUTHORIZATION);
         if (StringUtils.hasText(bearToken) && bearToken.startsWith(BEARER_PREFIX)) {
             return bearToken.substring(BEARER_PREFIX.length());
         }
